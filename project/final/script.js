301dce6b05026de77157c7ba077954b543bfd912
suyunbae <suybae@ucdavis.edu>
2018-05-14 01:05:31 -0700

    makersfaire

--- project/final/script.js
+++ project/final/script.js
@@ -50,19 +50,14 @@ function myFunction() {
   var d3 = document.getElementById('d3');
 
   var timeout = 1000;
+  var timeout2 = 2000;
   fadeOut(textarea, timeout);
   fadeOut(picture, timeout);
   fadeOut(button, timeout);
   fadeOut(title, timeout);
   fadeOut(welcome, timeout);
 
-
-
-
-  title.innerHTML = 'Analysis';
-
   setTimeout(function() {
-
     textarea.parentNode.removeChild(textarea);
     picture.parentNode.removeChild(picture);
     button.parentNode.removeChild(button);
@@ -72,7 +67,7 @@ function myFunction() {
     fadeIn(d1, 200);
     fadeIn(d3, timeout);
     fadeIn(title, timeout);
-
+    title.innerHTML = 'Analysis';
   }, timeout);
 
   var txtWithTag = "";
@@ -82,8 +77,8 @@ function myFunction() {
     var elmWithTag;
     if (highlight(elm)) {
 
-      if(elm.toLowerCase() == 'illegal' || elm.toLowerCase() == 'bitch'){
-        elmWithTag = "<a href=\"#\" class =\"foo link-1\"><span class='highlighted'  id=\"word\"  onclick=\"toggleHighlight()\">"+ elm + "</span></a>";
+      if(elm.toLowerCase() == 'illegal' || elm.toLowerCase() == 'bitch' || elm.toLowerCase() == 'ghetto' || elm.toLowerCase() == 'retarded' || elm.toLowerCase() == 'lame'|| elm.toLowerCase() == 'no homo' || elm.toLowerCase() == 'whore' || elm.toLowerCase() == 'hoe' ){
+        elmWithTag = "<a href=\"#\" class =\"foo link-1\"><span class='highlighted'  id=\"word\"  onclick=\"toggleHighlight(this)\">"+ elm + "</span></a>";
       }
       else {
         elmWithTag = "<span class='highlighted'>" + elm + "</span>";
@@ -110,16 +105,15 @@ function createElementFromHTML(htmlString) {
 }
 
 function highlight(word){
-  var result = word.match(/illegal|bitch/i);
+  var result = word.match(/illegal|bitch|ghetto|retarted|lame|whore|slut|hoe|no homo/i);
   return result;
   // console.log(result)
 }
 
 
 
-function toggleHighlight() {
-    // console.log("hi");
-    var word = document.getElementById("word");
+function toggleHighlight(thisDom) {
+    var word = thisDom;
     var link = document.getElementsByClassName("foo")[0];
     var explanation =document.getElementById("t-explanation");
 
@@ -128,14 +122,32 @@ function toggleHighlight() {
         word.classList.remove("highlight");
         explanation.innerHTML = "Click on any of the highlighted words to learn more about their connotations!";
     } else {
-        word.className += " highlight";
-        link.classList.remove("link-1");
+        // word.className += " highlight";
+        // link.classList.remove("link-1");
         link.style.textDecoration = "none";
         if(word.innerHTML == 'bitch'){
-          explanation.innerHTML = "The word <strong>\"bitch\"</strong> is a word which is used culturally to describe any woman who is outspoken, or strong, or uncompromising, in a derogatory manner.<br><br> To learn more, click <a href=\"https://www.bustle.com/articles/122081-the-one-sexist-microaggression-youre-probably-committing-daily-and-why-we-need-to-stop-using\">here";
+          explanation.innerHTML = "The word <strong>\"bitch\"</strong> is a word which is used culturally to describe any woman who is outspoken, or strong, or uncompromising, in a derogatory manner.";
         }
         else if(word.innerHTML == 'illegal'){
-          explanation.innerHTML = "The word <strong>\"illegal\"</strong> has been applied and abused by those advocating for harsh immigration policies that are racist and xenophobic. No one is illegal for living.<br><br> To learn more, click <a href=\"https://www.npr.org/sections/itsallpolitics/2013/01/30/170677880/in-immigration-debate-undocumented-vs-illegal-is-more-than-just-semantics\">here";
+          explanation.innerHTML = "The word <strong>\"illegal\"</strong> has been applied and abused by those advocating for harsh immigration policies that are racist and xenophobic. No one is illegal for living.";
+        }
+        else if(word.innerHTML == 'ghetto'){
+          explanation.innerHTML = "The word <strong>\"ghetto\"</strong> describes something or someone as cheap, worn out, poor, dangerous, etc. Reference to housing communities that are impoverished and disproportionately impact of people of color. Associates people of color with these negative characteristics";
+        }
+        else if(word.innerHTML == 'homo'){
+          explanation.innerHTML = "The word <strong>\"homo\"</strong> stigmatizes gay and queer people. Uses their identities to describe something as undesirable and bad. Replaces negative adjectives with words related to queer/LGBT identity.";
+        }
+        else if(word.innerHTML == 'retarted'){
+          explanation.innerHTML = "The word <strong>\"retarded\"</strong> targets mental, emotional, and physical disabilities as objects for ridicule. Used as synonyms for “worthless”, “bad”, “unintelligent”, “incapable”, etc";
+        }
+        else if(word.innerHTML == 'whore'){
+          explanation.innerHTML = "The word <strong>\"whore\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
+        }
+        else if(word.innerHTML == 'hoe'){
+          explanation.innerHTML = "The word <strong>\"hoe\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
+        }
+        else if(word.innerHTML == 'slut'){
+          explanation.innerHTML = "The word <strong>\"slut\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
         }
     }
 }
